78d77157c5db5e064f4b11e61931e45961dcccd6
Deborah <olubamiredeborah@gmail.com>
2025-07-03 15:09:02 +0100

    Initial commit: Morse code converter

---
+++ main.py
@@ -0,0 +1,27 @@
+morse_code_dict= {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----', ',': '--..--',
+    '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-',
+    '(': '-.--.', ')': '-.--.-', ' ': '/'
+}
+word=[]
+text=input('Enter the text that will be convert to morse code: ').upper()
+text_input=text
+
+
+
+for letter in text_input:
+    if letter in morse_code_dict:
+        word.append(morse_code_dict[letter])
+    else:
+        word.append(f"[{letter}]")
+
+
+text_join=' '.join(word)
+print(f'The morse code for {text} is {text_join}')
