ffd4ba477fd7d50c4c2a13a97c6efddc03a3765c
Deborah <olubamiredeborah@gmail.com>
2025-07-04 17:01:08 +0100

    morse code converter

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+morse=True
 morse_code_dict= {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
     'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
@@ -10,17 +11,30 @@ morse_code_dict= {
     '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-',
     '(': '-.--.', ')': '-.--.-', ' ': '/'
 }
-word=[]
-text=input('Enter the text that will be convert to morse code: ').upper()
-text_input=text
 
 
-for letter in text_input:
-    if letter in morse_code_dict:
-        word.append(morse_code_dict[letter])
+while morse:
+
+    word = []
+
+    text=input('Enter the text that will be convert to morse code: ').upper()
+
+    for letter in text:
+        if letter in morse_code_dict:
+            word.append(morse_code_dict[letter])
+        else:
+         word.append(f"{letter}")
+
+
+    text_join=' '.join(word)
+    print(f'The morse code for {text} is {text_join}')
+
+    repeat_morse=input('Do you wan to repeat morse Yes or No : ').lower()
+
+    if repeat_morse == 'yes' :
+        print('\n\n')
+        morse=True
+
     else:
-        word.append(f"[{letter}]")
-
-
-text_join=' '.join(word)
-print(f'The morse code for {text} is {text_join}')
+     print('\n\n Good bye 🖐')
+     morse=False
